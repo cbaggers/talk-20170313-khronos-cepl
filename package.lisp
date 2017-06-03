937250143c0761709805a4466f185a7772ce2da3
Baggers <chris.bagley@gmail.com>
2017-06-03 15:22:40 +0200

    move to vari

--- package.lisp
+++ package.lisp
@@ -1,7 +1,7 @@
 ;;;; package.lisp
 
 (uiop:define-package #:talk
-    (:use #:cl #:cepl #:named-readtables #:varjo-lang #:rtg-math
+    (:use #:cl #:cepl #:named-readtables #:vari
           :rtg-math.base-maths :rtg-math.types
           #:skitter.sdl2.keys #:skitter.sdl2.mouse-buttons
           #:nineveh))
